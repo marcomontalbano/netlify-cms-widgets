a7e3729dcb96d2afa8886b237f9ff25d6e50de6a
Marco Montalbano <marcomontalbano.work@gmail.com>
2021-02-04 08:10:34 +0100

    Remove package netlify-cms-json-exporter.
    Package has been moved in a separate repository.
    https://github.com/marcomontalbano/netlify-cms-json-exporter

--- example.json-exporter.ts
+++
@@ -1,6 +0,0 @@
-import fs from 'fs'
-import { getContent } from './packages/netlify-cms-json-exporter/src'
-
-const content = getContent('./playground/config.yml', './content')
-
-fs.writeFileSync('db.json', JSON.stringify(content, undefined, 2))
